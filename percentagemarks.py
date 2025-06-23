66f6862fc70dfb1d25e249a2af24b6e158254766
gishel1 <2719gram@gmail.com>
2025-06-23 18:27:29 +0100

    count the notes

---
+++ percentagemarks.py
@@ -0,0 +1,7 @@
+maths = int(input("maths marks are:"))
+english = int(input("english marks are:"))
+science = int(input("science marks are:"))
+history = int(input("history marks are:"))
+
+totalpercentage = (((maths + english + science + history)/400)*100)
+print("the total percentage is", totalpercentage, "%")
