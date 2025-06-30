c25aff3248b61412762f02046257895ceb5dcffe
gishel1 <2719gram@gmail.com>
2025-06-30 18:20:40 +0100

    bmichecker

---
+++ bmichecker.py
@@ -0,0 +1,15 @@
+height = int(input("enter your height in cm"))
+weight = int(input("enter your weight in kg"))
+bmi = ((weight/ (height*height)) *100)
+if bmi <= 18.4:
+    print("you are underweight")
+elif bmi <= 24.9:
+    print("you are healthy")
+elif bmi <= 29.9:
+    print("you are overweight")
+elif bmi <= 34.9:
+    print("you are severely overweight")
+elif bmi <= 39.9:
+    print("you are obese")
+else:
+    print("you are severely obese")
