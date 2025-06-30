20f28a23565777ce932277595a58ef6cfedca142
gishel1 <2719gram@gmail.com>
2025-06-30 17:28:23 +0100

    squared

---
+++ squared.py
@@ -0,0 +1,6 @@
+print("welcome to the square root finder!")
+number = input("enter a number to find out its square root")
+newnumber = float(number)
+import math
+answer = math.sqrt(newnumber)
+print("the square root of your number is", answer, "!")
