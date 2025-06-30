2549ceeee3e6c96f26fbdc1bc767e40d15667423
gishel1 <2719gram@gmail.com>
2025-06-30 18:01:34 +0100

    greaterthanorsmallerthan

---
+++ greaterorsmallthan.py
@@ -0,0 +1,8 @@
+num = int(input("enter your number"))
+if(num > 15):
+    print("the number is greater than 15!")
+    print("I am in the if block!")
+else:
+    print("the number is smaller than 15!")
+    print("I am in the else block!")
+print("I am not in the if block or the else block!!")
