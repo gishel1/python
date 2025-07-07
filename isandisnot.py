f59ca1c361966ab6bfdcc1f8b60b3b45f87b61c5
gishel1 <2719gram@gmail.com>
2025-07-07 18:12:53 +0100

    isandisnot

---
+++ isandisnot.py
@@ -0,0 +1,12 @@
+x = 5.0
+if type(X) is int:
+    print("x is an integer")
+else:
+    print("x is not an integer")
+
+y = 5.0
+if type(y) is not int:
+    print("y is not an integer")
+else:
+    print("y is an integer")
+    
